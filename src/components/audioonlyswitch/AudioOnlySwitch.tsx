779419928c24ce356afd327d1db94933af86bc67
Michele De Palma <depalma_michele@outlook.it>
2022-12-15 10:11:36 +0100

    Created AudioOnlySwitch component

---
+++ src/components/audioonlyswitch/AudioOnlySwitch.tsx
@@ -0,0 +1,48 @@
+import { SpatialAudio } from '@mui/icons-material';
+import { FormControlLabel, styled, Switch } from '@mui/material';
+import { audioOnlyModeLabel } from '../translated/translatedComponents';
+
+interface AudioOnlyProps {
+	checked: boolean;
+	// eslint-disable-next-line
+	onChange: (event: React.ChangeEvent<HTMLInputElement>) => void;
+}
+
+const StyledDiv = styled('div')(({ theme }) => ({
+	width: '100%',
+	display: 'flex',
+	padding: theme.spacing(1, 0),
+	'.MuiSvgIcon-root': {
+		margin: 'auto'
+	}
+}));
+
+const StyledFormControlLabel = styled(FormControlLabel)(({ theme }) => ({
+	width: '100%',
+	justifyContent: 'space-between',
+	marginLeft: theme.spacing(1)
+}));
+
+const AudioOnlySwitch = ({
+	checked,
+	onChange
+}: AudioOnlyProps): JSX.Element => {
+	return (
+		<StyledDiv>
+			<SpatialAudio />
+			<StyledFormControlLabel
+				control={
+					<Switch
+						color='primary'
+						checked={ checked }
+						onChange={ onChange }
+					/>
+				}
+				label={ audioOnlyModeLabel() }
+				labelPlacement='start'
+			/>
+		</StyledDiv>
+	);
+};
+
+export default AudioOnlySwitch;
