5bda854b6bedbd2de14cf4b5cfa675e4273e55bb
Michele De Palma <depalma_michele@outlook.it>
2023-05-02 08:59:09 +0200

    Ranamed privacyOpen field into privacyDialogOpen

--- src/store/slices/uiSlice.tsx
+++ src/store/slices/uiSlice.tsx
@@ -15,7 +15,7 @@ export interface UiState {
 	aboutOpen: boolean;
 	lobbyDialogOpen: boolean;
 	extraVideoDialogOpen: boolean;
-	privacyOpen: boolean;
+	privacyDialogOpen: boolean;
 	currentSettingsTab: SettingsTab;
 	currentHelpTab: HelpTab;
 	showStats: boolean;
@@ -34,7 +34,7 @@ const initialState: UiState = {
 	aboutOpen: false,
 	lobbyDialogOpen: false,
 	extraVideoDialogOpen: false,
-	privacyOpen: false,
+	privacyDialogOpen: false,
 	currentSettingsTab: 'media',
 	currentHelpTab: 'shortcuts'
 };
