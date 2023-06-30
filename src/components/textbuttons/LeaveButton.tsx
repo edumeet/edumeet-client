56ba02bdf857bc8ae3778294bbd119966a4fa7fb
Pontus Fagerström <pontus.fagerstrom@fripost.org>
2023-06-30 07:32:04 +0200

    fix: use window.location instead of react router

--- src/components/textbuttons/LeaveButton.tsx
+++ src/components/textbuttons/LeaveButton.tsx
@@ -1,16 +1,14 @@
 import { Button } from '@mui/material';
-import { useNavigate } from 'react-router-dom';
 import { leaveLabel } from '../translated/translatedComponents';
 
 const LeaveButton = (): JSX.Element => {
-	const navigate = useNavigate();
 
 	return (
 		<Button
 			aria-label={leaveLabel()}
 			color='error'
 			variant='contained'
-			onClick={() => navigate('/')}
+			onClick={() => location.replace(window.location.href.split('?')[0])}
 			size='small'
 		>
 			{ leaveLabel() }
