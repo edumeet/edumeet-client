d7703330da4986c0d31bb5ee8214a455151c291f
Pontus Fagerström <pontus.fagerstrom@fripost.org>
2023-08-29 08:32:19 +0200

    persist previewaudiooutputdeviceid

--- src/store/store.tsx
+++ src/store/store.tsx
@@ -73,7 +73,7 @@ const persistConfig = {
 const mediaPersistConfig = {
 	key: 'edumeetMedia',
 	storage,
-	whitelist: [ 'previewVideoDeviceId', 'previewAudioInputDeviceId' ]
+	whitelist: [ 'previewVideoDeviceId', 'previewAudioInputDeviceId', 'previewAudioOutputDeviceId' ]
 };
 
 const signalingService = new SignalingService();
