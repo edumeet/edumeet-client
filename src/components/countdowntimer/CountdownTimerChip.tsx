d68d0a3b174da4e05ea1db02fdd6aee30c7245d8
Roman Drozd <roman@drozd.it>
2024-04-16 15:17:30 +0200

    Clean code

--- src/components/countdowntimer/CountdownTimerChip.tsx
+++ src/components/countdowntimer/CountdownTimerChip.tsx
@@ -1,24 +1,11 @@
-import React, { useRef } from 'react';
-import { Chip, styled } from '@mui/material';
-import { HighlightOff as HighlightOffIcon, Pause as PauseIcon } from '@mui/icons-material';
-import { useAppDispatch, useAppSelector } from '../../store/hooks';
-// import { intl } from '../../utils/intlManager';
-import * as countdownTimerActions from '../../store/actions/countdownTimerActions';
-import { useTheme } from '@mui/material/styles';
+import React from 'react';
+import { Chip } from '@mui/material';
+import { useAppSelector } from '../../store/hooks';
 import AvTimerIcon from '@mui/icons-material/AvTimer';
 
-// const Div = styled('div')(({ theme }) => ({
-// 	padding: theme.spacing(1),
-// 	marginRight: theme.spacing(1),
-// 	marginTop: theme.spacing(1)
-// }));
-
 const CountdownTimerChip = () : JSX.Element => {
-	const dispatch = useAppDispatch();
 	const isEnabled = useAppSelector((state) => state.countdownTimer.isEnabled);
-	// const isRunning = useAppSelector((state) => state.countdownTimer.isRunning);
 	const left = useAppSelector((state) => state.countdownTimer.left);
-	const theme = useTheme();
 
 	return (
 		<>
@@ -30,31 +17,11 @@ const CountdownTimerChip = () : JSX.Element => {
 				}}
 				label={left}
 				size='small'
-				icon={<AvTimerIcon color='white'/>}
+				icon={<AvTimerIcon style={{ color: 'white' }}/>}
 			/>
 			}
 		</>
 	);
 };
 
-// const mapStateToProps = (state) => ({
-// 	isEnabled: state.room.countdownTimer.isEnabled,
-// 	isRunning: state.room.countdownTimer.isRunning,
-// 	left: state.room.countdownTimer.left
-// });
-
-// export default withRoomContext(connect(
-// 	mapStateToProps,
-// 	null,
-// 	null,
-// 	{
-// 		areStatesEqual: (next, prev) => {
-// 			return (
-// 				prev.isEnabled === next.room.countdownTimer.isEnabled,
-// 				prev.isRunning === next.room.countdownTimer.isRunning,
-// 				prev.left === next.room.countdownTimer.left
-// 			);
-// 		}
-// 	}
-// )(withStyles(styles)(CountdownTimer)));
 export default CountdownTimerChip;
