f484dbabddd27b159c19a3e817a42ea8cdb3b80c
Roman Drozd <roman@drozd.it>
2024-04-16 19:23:04 +0200

    Clean code

--- src/store/middlewares/countdownTimerMiddleware.tsx
+++ src/store/middlewares/countdownTimerMiddleware.tsx
@@ -4,7 +4,6 @@ import { Logger } from '../../utils/Logger';
 import { countdownTimerActions } from '../slices/countdownTimerSlice';
 import { signalingActions } from '../slices/signalingSlice';
 import { AppDispatch, MiddlewareOptions, RootState } from '../store';
-import moment from 'moment';
 
 const logger = new Logger('ChatMiddleware');
 
@@ -20,9 +19,6 @@ const createCountdownTimerMiddleware = ({
 		getState: () => RootState
 	}) =>
 		(next) => (action) => {
-
-			const countdownTimer = getState().countdownTimer;
-			let _countdownTimerRef : any;
 
 			if (signalingActions.connect.match(action)) {
 				signalingService.on('notification', (notification) => {
@@ -46,49 +42,10 @@ const createCountdownTimerMiddleware = ({
  
 								dispatch(countdownTimerActions.startCountdownTimer());
 
-								// clearInterval(_countdownTimerRef);
-
-								// _countdownTimerRef = setInterval(() => {
-								// 	let left = moment(`1000-01-01 ${countdownTimer.left}`).unix();
-								// 	const end = moment('1000-01-01 00:00:00').unix();
-				
-								// 	left--;
-					
-								// 	const left2 = moment.unix(left).format('HH:mm:ss');
-
-								// 	dispatch(countdownTimerActions.setCountdownTimer({ left: left2 }));
-
-								// 	// countdownTimer.left = moment.unix(left).format('HH:mm:ss');
-					
-								// 	// if (left === end) {
-								// 	// 	// if (left === end || room.empty) {
-								// 	// 	clearInterval(_countdownTimerRef);
-					
-								// 	// 	dispatch(countdownTimerActions.stopCountdownTimer());
-								// 	// 	dispatch(countdownTimerActions.setCountdownTimer({ left: '00:00:00' }));
-					
-								// 	// 	// countdownTimer.isRunning = false;
-								// 	// 	// countdownTimer.left = '00:00:00';
-				
-								// 	// 	// room.notifyPeers('moderator:setCountdownTimer', {
-								// 	// 	// 	peerId: peer.id,
-								// 	// 	// 	left: countdownTimer.left,
-								// 	// 	// 	// isRunning: countdownTimer.isRunning
-								// 	// 	// });
-				
-								// 	// 	// room.notifyPeers('moderator:stoppedCountdownTimer', {
-								// 	// 	// 	peerId: peer.id,
-								// 	// 	// 	isRunning: countdownTimer.isRunning
-								// 	// 	// });
-								// 	// }
-					
-								// }, 1000);
-
 								break;
 							}
 
 							case 'moderator:stoppedCountdownTimer': {
-								// const { isEnabled } = notification.data;
  
 								dispatch(countdownTimerActions.stopCountdownTimer());
 
@@ -102,20 +59,6 @@ const createCountdownTimerMiddleware = ({
 								dispatch(countdownTimerActions.setCountdownTimer(
 									{ left, isRunning }));
 		
-								// if (arr.includes(left) && isRunning) {
-								// if (left == arr[0] && isRunning) {
-								// 	dispatch(countdownTimerActions.notify(
-								// 		{
-								// 			type: 'info',
-								// 			text: intl.formatMessage({
-								// 				id: 'xxx',
-								// 				defaultMessage: 'Time is up'
-								// 			})
-								// 		}));
-		
-								// 	this._soundNotification('countdownTimer');
-								// }
-
 								break;
 							}
 						}
