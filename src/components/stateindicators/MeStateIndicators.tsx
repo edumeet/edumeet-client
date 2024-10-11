4080afdf17ff08a0bbc8fdde88ed76bcf63683dc
Johannes Jeppsson <johannes@sunet.se>
2024-10-11 10:30:58 +0200

    Unmute alert at top and show micOff for me

--- src/components/stateindicators/MeStateIndicators.tsx
+++ src/components/stateindicators/MeStateIndicators.tsx
@@ -3,12 +3,12 @@ import RaiseHand from '@mui/icons-material/BackHand';
 import { useAppSelector } from '../../store/hooks';
 
 const MeStateIndicators = (): JSX.Element => {
-	const micEnabled = useAppSelector((state) => state.me.micEnabled);
+	const audioMuted = useAppSelector((state) => state.me.audioMuted);
 	const raisedHand = useAppSelector((state) => state.me.raisedHand);
 
 	return (
 		<>
-			{ !micEnabled && <MicOff color='error' fontSize='small' /> }
+			{ audioMuted && <MicOff color='error' fontSize='small' /> }
 			{ raisedHand && <RaiseHand fontSize='small' /> }
 		</>
 	);
