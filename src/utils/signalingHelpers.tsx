f2db80c7757a5c498c026aea0bf993ffe13c7f43
Piotr Pawałowski <piotr.pawalowski@medvc.eu>
2026-02-18 14:13:53 +0100

    Fix that somtimes the tenant id is unknow, now room server checks

--- src/utils/signalingHelpers.tsx
+++ src/utils/signalingHelpers.tsx
@@ -5,21 +5,18 @@ import edumeetConfig from './edumeetConfig';
  * 
  * @param peerId - The id of this client.
  * @param roomId - The id of the room.
- * @param tenantId - The id of the tenant.
  * @param token - The token of the user.
  * @returns {string} The url of the signaling server.
  */
-export const getSignalingUrl = (peerId: string, roomId: string, tenantId: string | undefined, token: string | undefined): string => {
+export const getSignalingUrl = (peerId: string, roomId: string, token: string | undefined): string => {
 	const hostname = edumeetConfig.serverHostname || window.location.hostname;
 	const port = import.meta.env.PROD ? edumeetConfig.productionPort : edumeetConfig.developmentPort;
 
-	let tenantParam = '';
 	let tokenParam = '';
 
-	if (tenantId) tenantParam = `&tenantId=${tenantId}`;
 	if (token) tokenParam = `&token=${token}`;
 
-	return `wss://${hostname}:${port}/?peerId=${peerId}&roomId=${roomId}${tenantParam}${tokenParam}`;
+	return `wss://${hostname}:${port}/?peerId=${peerId}&roomId=${roomId}${tokenParam}`;
 };
 
 export class SocketTimeoutError extends Error {
